81fc310d9b6302f6ac170f39cbb43d2c3e4b1c5b
Leon Vladimar <phanideepak1729@gmail.com>
2023-02-05 12:04:45 +0530

    print primes between two numbers

--- mathematics/Maths.java
+++ mathematics/Maths.java
@@ -35,6 +35,57 @@ class Maths{
         return a[n]; 
     }
 
+    public int numberFactors(int n){
+        int count =0;
+        
+        for(int i=1;i<=n;i++) if(n%i==0) count = count + 1;
+        
+        return count;
+    }
+
+    // outdate and noob logic.
+    public void checkPrime(int n){
+        int count = 0;
+        for(int i = 1; i<=n;i++){
+            if(n%i == 0) count = count + 1;
+        }
+    
+        if(count == 2) {
+           System.out.println("Prime number");
+        } else if (count == 1) {
+            System.out.println("given number is one");     
+        } else {
+           System.out.println("composite number");
+        }
+    }
+
+    public void checkPrimeOrNot(int n){
+        int i;
+
+        for(i = 2; i<n;i++){
+            if(n%i == 0){
+                System.out.println("non prime");
+            }
+        }
+        if(i == n){
+            System.out.println("prime");
+        }
+    }
+
+    public void printPrimes(int a, int b){
+       for(int num = a; num<=b ; num++){
+           int i;
+           for(i = 2; i <num;i++){
+              if(num % i ==0) {
+                break;
+              }
+           }
+           if(i == num ){
+               System.out.println("Prime number: "+i);
+           }
+       }   
+    }
+
     public static void main(String[] args){
         Maths m = new Maths();
         System.out.println(m.greaterFive(22,21,88,88,87));
@@ -42,5 +93,8 @@ class Maths{
 
         System.out.println("factorial : "+m.factorial(30));
         System.out.println("fibonacci : "+ m.fibonacci(10));
+        System.out.println("number of factors : "+m.numberFactors(720));
+        m.checkPrimeOrNot(1117);
+        m.printPrimes(1,100);
     }
 }
