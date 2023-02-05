d26e7619836456bdf97ba90a313ba4d57c2a9933
Leon Vladimar <phanideepak1729@gmail.com>
2023-02-05 15:26:57 +0530

    print pattern

--- operator/Operators.java
+++ operator/Operators.java
@@ -11,7 +11,32 @@ class Operators{
         System.out.println(k);
         System.out.println("end of problem 1 - i :"+i);
     }
+
+    static void problem2(){
+        int i = 20;
+        // i = 21, 22
+        i = i++ + ++i;
+        System.out.println("end of problem2 - i: "+i);
+    }
+
+    static void problem3(){
+        int i = 20;
+        int j = 40;
+        int k = i + j + i ++ + j ++ + ++i + ++j;
+        System.out.println(String.format("problem 3 - i : %s j: %s  k: %s",i,j,k));
+    }
+
+    static void problem4(){
+        int i  = 10;
+        int j = 20;
+        int k = i-- - i++ + --j - ++j + --i - j-- + ++i - j++;
+        System.out.println(String.format("problem 4 - i : %s  j:%s  k: %s",i,j,k));
+    }
+
     public static void main(String[] args){
          problem1();
+         problem2();
+         problem3();
+         problem4();
     }
 }
---
+++ print-pattern/PrintPattern.java
@@ -0,0 +1,132 @@
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+class PrintPattern{
+
+    static void pattern1(){
+        int n = 5;
+        int m = 7;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void hollowReactangularPattern(){
+        System.out.println();
+        System.out.println();
+        int n = 5;
+        int m = 7;
+        for(int i=1; i<=n;i++){
+            for(int j=1; j<=m;j++){
+                if(i==1 || i == n){
+                    System.out.print("*");
+                } else if (j==1 || j==m){
+                  System.out.print("*");
+                }else{
+                    System.out.print("@");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    static void halfInvertedPyramid(){
+        System.out.println();
+        System.out.println();
+        int n = 5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i+1;j++){
+              System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void halfPyramidAfter180(){
+        System.out.println();
+        int n = 5;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n;j++){
+               // restrict j upperbound
+               if(j <= n-i)
+                System.out.print(" ");
+               else 
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    static void halfPyramidUsingNumbers(){
+        int n = 6;
+        System.out.println();
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+               System.out.print(i);
+            }
+            System.out.println();
+        }
+    }
+
+    static void floydTraingle(){
+        int n = 6;
+        System.out.println();
+        int count = 0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                count = count + 1;
+                System.out.print(count+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void butterFlyPattern(){
+        int n = 4;
+        // max row size = 2*n, min row size = 2 *n
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+
+            for(int j=1;j<=2*n-2*i;j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+
+            for(int j=1;j<=2*n-2*i;j++){
+                System.out.print(" ");
+            }
+
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+
+    public static void main(String[] args){
+        pattern1();
+        hollowReactangularPattern();
+        halfInvertedPyramid();
+        halfPyramidAfter180();
+        halfPyramidUsingNumbers();
+        floydTraingle();
+        butterFlyPattern();
+    }
+}
