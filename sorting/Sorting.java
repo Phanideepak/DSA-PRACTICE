fab79fc0e28098903c22b3f5f2b19582491de3df
Leon Vladimar <phanideepak1729@gmail.com>
2023-02-06 18:30:21 +0530

    insertion sort changes

--- sorting/Sorting.java
+++ sorting/Sorting.java
@@ -26,16 +26,55 @@ class Sorting{
         System.out.println();
     }
 
-    public static void main(String[] args){
-        int n;
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        int []a = new int[n];
-
-        for(int i = 0; i<n;i++){
-            a[i] = sc.nextInt();
+    static void bubbleSort(int [] a , int n){
+        // This algo always runs for O(n*n)
+        for(int i =0; i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(a[i]>a[j]){
+                    int temp = a[i];
+                    a[i] =  a[j];
+                    a[j] = temp;
+                }
+            }
         }
 
-        selectionSort(a,n);
+        for(int i = 0; i<n; i++){
+           System.out.print(a[i]+" ");
+        }
+        System.out.println();
+    }
+
+    static void bubbleSortOptimised(int[] a, int n){
+        boolean swap;
+        for(int i=0;i<n-1;i++){
+            swap = false;
+            for(int j=0;j<n-i-1;j++){
+                if(a[j]>a[j+1]){
+                    int temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                    swap = true;
+                }
+            }
+            if(!swap) break;
+        }
+
+        for(int i = 0; i<n; i++){
+           System.out.print(a[i]+" ");
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for(int i = 0 ;i<n;i++){
+            a[i] = sc.nextInt();
+        }
+        
+        bubbleSortOptimised(a,n);
     }
 }
