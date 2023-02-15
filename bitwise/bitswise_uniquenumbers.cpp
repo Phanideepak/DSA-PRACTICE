2dffd66ebcdd86e2f29466ac5364cdda87066a9d
Leon Vladimar <phanideepak1729@gmail.com>
2023-02-15 13:18:30 +0530

    changes-1

--- bitwise/bitswise_uniquenumbers.cpp
+++ bitwise/bitswise_uniquenumbers.cpp
@@ -54,6 +54,44 @@ void findTwoUniqueNumberInArray(int arr[], int n){
     cout<<"firstNumber : "<<firstNumber<<"secondNumber : "<<secondNumber<<endl;
 }
 
+
+/*
+  Find unique number in array where all numbers except those one are present thrice
+*/
+
+/*
+*  Logic:
+
+*  1. Create an frequency array of size 64 that holds frequency of setbit(1) belongs to array(size n) elements
+*  2. If frequency is divisible by 3, It means that setbit is repeated thrice. Ignore that setbit.
+      Else answer = answer | (1<<pos_index)
+*  3. print(answer)
+*  
+*/
+
+void findUniqueNumberInArrayRepeatedThrice(int arr[], int n){
+    cout<<"code entry point";
+    int num = 0;
+    int frequency = 0;
+
+    for(int i=0;i<64;i++){
+        frequency=0;
+        for(int j=0;j<n;j++){
+            if(isOneBitExists(arr[j],i)){
+                frequency++;
+            }
+        }
+        // If frequency is not divisble by 3, then setbit at position i is considered.
+        if(frequency%3 !=0) {
+            num = num | (1<<i);
+        }
+    }
+    cout<<"answer: "<<num<<endl;    
+      
+}
+
+
+
 int main(){
     int n;
     cin>>n;
@@ -61,4 +99,6 @@ int main(){
     for(int i=0;i<n;i++) cin>>arr[i];
     //cout<<findUniqueNumberInArray(arr,n)<<endl;
     //findTwoUniqueNumberInArray(arr,n);
+
+    findUniqueNumberInArrayRepeatedThrice(arr,n);
 }
