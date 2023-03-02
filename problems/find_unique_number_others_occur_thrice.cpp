dcf171b76ca3b99ae4a02dd7076a1746a622e0ac
Leon Vladimar <phanideepak1729@gmail.com>
2023-03-02 17:24:50 +0530

    unique number in array of occurence thrice.

--- problems/find_unique_number_others_occur_thrice.cpp
+++ problems/find_unique_number_others_occur_thrice.cpp
@@ -6,6 +6,21 @@ int getBit(int n, int posIndex){
     return (n & (1 << posIndex)) > 0;
 }
 
+/**
+ *  Problem statement: All the elements except one occur thrice in an array. Find out that unique number.
+ 
+ *  Solution:
+ * 1. Create a frequency array of size 64 that holds the frequency of setbits at a particular index position for 
+ *    all the array elements.
+ * 2. If frequency at a index is not divisible by 3 then setbit at that index belongs to unique number.
+ * 3. Unique number can be formed by setting the identified setbits.
+ * Logic: answer = answer | 1<< p (p is the position where desired setbit is identified)
+ 
+
+ * Logic 1: answer = answer + 1<<p. 
+ * Failed: Logic 1 fails due to memory over flow issues. 
+*/
+
 int main(){
     int n;
     cin>>n;
@@ -22,8 +37,7 @@ int main(){
              }
         }
         if(frequency%3 !=0){
-            answer = answer + 1<<i;
-            cout<<i<<" "<<answer<<" "<<frequency<<endl;
+            answer = answer | (1<<i);
         }
     }
     cout<<answer<<endl;
